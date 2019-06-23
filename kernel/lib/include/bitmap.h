092b323d272a2e45616687ba06eccc11567223a7
monoid <qaswedfr55@gmail.com>
2019-06-23 10:47:49 +0000

    단순 비트맵

---
+++ kernel/lib/include/bitmap.h
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdint.h>
+
+namespace util
+{
+class bitmap{
+public:
+    struct proxy{
+        uint64_t * data;
+        size_t index;
+        bool operator=(bool value){
+            if (value){
+                data[(index >> 6)] |= (1<< (index&0x3f));
+            }
+            else{
+                data[(index >> 6)] &= ~(1<< (index&0x3f));
+            }
+            return value;
+        }
+    };
+    bool operator[](size_t index) const{
+        return data[(index >> 6)] & (1<< (index&0x3f));
+    }
+    proxy operator[](size_t index){
+        return proxy{data,index};
+    }
+private:
+    uint64_t * data;
+};
+} // util
