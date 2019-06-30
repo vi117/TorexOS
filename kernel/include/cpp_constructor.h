43d75d3620bff1b03bce7c2be71f5319b3aee586
monoid <qaswedfr55@gmail.com>
2019-06-30 08:31:20 +0000

    생성자 호출 성공(버그 고침)

--- kernel/include/cpp_constructor.h
+++ kernel/include/cpp_constructor.h
@@ -1,20 +1,26 @@
 #ifndef _CPP_CONSTRUCTOR_H_
 #define _CPP_CONSTRUCTOR_H_
 
+#define NoOptimize __attribute__((optimize("O0")))
+
 typedef void(*func_ptr)(void);
 
 extern func_ptr __init_array_start, __init_array_end;
 extern func_ptr __fini_array_start, __fini_array_end;
- 
-void call_constructor(void)
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+extern "C"{
+void _init(void) 
 {
-	for ( func_ptr* func = &__init_array_start; func != &__init_array_start; func++ )
-		(*func)();
+	for ( func_ptr* func = &__init_array_start; func != &__init_array_end; func++ )
+		(**func)();
 }
  
-void call_destructor(void)
+void _fini(void)
 {
-	for ( func_ptr* func = &__fini_array_end; func != &__fini_array_end; func++ )
-		(*func)();
+	for ( func_ptr* func = &__fini_array_start; func != &__fini_array_end; func++ )
+		(**func)();
 }
+}
+#pragma GCC pop_options 
 #endif
--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -1,9 +1,16 @@
 #include <vga/vgaout.h>
 #include <cpp_constructor.h>
 
+class sss{
+public:
+sss(){
+    text::raw_print(0,1,"AAAAA");
+}
+};
+volatile sss s;
 int main()
 {
-    call_constructor();
+    _init();
     text::initialize();
     text::puts("Hello, World!\n");
     text::puts("aaaa");
