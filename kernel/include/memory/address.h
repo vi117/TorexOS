5c6be5eb45755cfab65e607c8fe852e4801e98b8
monoid <qaswedfr55@gmail.com>
2019-07-06 06:21:22 +0000

    add literal operator _MB and _KB for convenience

--- kernel/include/memory/address.h
+++ kernel/include/memory/address.h
@@ -2,6 +2,7 @@
 
 #ifndef _ADDRESS_H_
 #define _ADDRESS_H_
+inline namespace address{
 
 using phys_addr_t = uintptr_t;
 using ker_addr_t = uintptr_t;
@@ -10,4 +11,12 @@ inline constexpr ker_addr_t phys_to_ker(phys_addr_t addr){
     return (addr|0xffff800000000000);
 }
 
+inline constexpr unsigned long long operator "" _KB (unsigned long long a){
+    return a*1024;
+}
+inline constexpr unsigned long long operator "" _MB (unsigned long long a){
+    return a*1024*1024;
+}
+
+} // address
 #endif
