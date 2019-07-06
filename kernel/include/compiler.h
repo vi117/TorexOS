9e07d74694450533d9d14ed5521876245ce03dc5
monoid <qaswedfr55@gmail.com>
2019-07-06 06:18:24 +0000

    compiler specific functions

---
+++ kernel/include/compiler.h
@@ -0,0 +1,6 @@
+
+#define __GET_CALLER(x) __builtin_return_address(0)
+#define __GET_FRAME(x)  __builtin_frame_address(0)
+
+#define unlikely(x) __builtin_expect((x), 0)
+#define likely(x) __builtin_expect((x), 1)
