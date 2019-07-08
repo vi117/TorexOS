a7df1abb17e71d96fa6bab2dd11895cfd73126d5
monoid <qaswedfr55@gmail.com>
2019-07-08 11:02:38 +0000

    [ADD] linear_allocator

---
+++ kernel/lib/include/linear_allocator.h
@@ -0,0 +1,30 @@
+#include <stddef.h>
+
+#ifndef _LINEAR_ALLOCATOR_H_
+#define _LINEAR_ALLOCATOR_H_
+
+namespace util
+{
+    template<typename Ty>
+    class LinearAllocator{
+    public:
+        explicit LinearAllocator(Ty * start_ptr ,Ty * end_ptr )
+            :addr(start_ptr),end(end_ptr){}
+        Ty * allocate(size_t n){
+            auto ret = addr;
+            addr += n;
+            if(addr => end){
+                addr = ret;
+                return nullptr;
+            }
+            return ret;
+        }
+        void deallocate(Ty *){/*do nothing.*/}
+    private:
+        Ty * addr;
+        Ty * end;
+    };
+} // util
+
+
+#endif
