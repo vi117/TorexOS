31ec46a47eee2d8ca6d283e0dd01bf6fbe89b729
monoid <qaswedfr55@gmail.com>
2019-08-11 06:40:52 +0000

    [FIX] 편의성 증대

--- kernel/lib/include/alloc_policy/pool_allocator.h
+++ kernel/lib/include/alloc_policy/pool_allocator.h
@@ -4,35 +4,51 @@
 
 namespace util
 {
-    template<typename Ty>
-    class PoolAllocator{
-    public:
-        constexpr explicit PoolAllocator():head(nullptr){}
-        struct Node{
-            union
-            {
-                Node * next;
-                Ty dummy;
-            };
-        };
-        Ty * allocate(){
-            return pop();
-        }
-        void deallocate(Ty * ptr){
-            push(ptr);
-        }
-        void push(Ty * ptr){
-            auto new_head = reinterpret_cast<Node *>(ptr);
-            new_head->next = head;
-            head = new_head;
-        }
-        Ty * pop(){
-            auto ret = head;
-            if(ret == nullptr) return nullptr;
-            head = head->next;
-            return &ret->dummy;
-        }
-    private:
-        Node * head;
+template <typename Ty>
+class PoolAllocator
+{
+public:
+    constexpr explicit PoolAllocator() : head(nullptr) {}
+    union Node {
+        Node *next;
+        Ty dummy;
     };
-} // util
+    inline Ty *allocate()
+    {
+        return pop();
+    }
+    inline void deallocate(Ty *ptr)
+    {
+        push(ptr);
+    }
+    void push(Ty *ptr)
+    {
+        auto new_head = reinterpret_cast<Node *>(ptr);
+        new_head->next = head;
+        head = new_head;
+    }
+    Ty *pop()
+    {
+        auto ret = head;
+        if (ret == nullptr)
+            return nullptr;
+        head = head->next;
+        return &ret->dummy;
+    }
+    void add_memblock(Node *start, size_t length)
+    {
+        const auto count = length / sizeof(Node);
+        if(count > 0){
+            for (size_t i = 0; i < count; i++)
+            {
+                start[i].next = & start[i+1];
+            }
+            start[count - 1].next = head;
+            head = start;
+        }
+    }
+
+private:
+    Node *head;
+};
+} // namespace util
