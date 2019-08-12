3e584fb63fd7608a96ffb40340ef11b8298ab1e3
monoid <qaswedfr55@gmail.com>
2019-08-12 05:28:02 +0000

    [FIX] simple change

--- kernel/include/intrins.h
+++ kernel/include/intrins.h
@@ -1,11 +1,12 @@
 #include<stdint.h>
 #include<memory/address.h>
 #include<immintrin.h>
+#include<compiler.h>
 
 #ifndef _INTRINS_H_
 #define _INTRINS_H_
 
-static inline void outbyte(uint16_t port, uint8_t val)
+static force_inline void outbyte(uint16_t port, uint8_t val)
 {
     asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
     /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
@@ -13,7 +14,15 @@ static inline void outbyte(uint16_t port, uint8_t val)
      * The  outb  %al, %dx  encoding is the only option for all other cases.
      * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
 }
-static inline void loadgdtr(phys_addr_t base,uint16_t limit)
+static force_inline uint8_t inbyte(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ( "inb %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+static force_inline void loadgdtr(phys_addr_t base,uint16_t limit)
 {
 #pragma pack(push,1)
     struct{
@@ -24,11 +33,11 @@ static inline void loadgdtr(phys_addr_t base,uint16_t limit)
     asm volatile ( "lgdt %0" : : "m"(gdtr));
 #pragma pack(pop)
 }
-static inline void loadtr(uint16_t offset)
+static force_inline void loadtr(uint16_t offset)
 {
     asm volatile ( "ltr %0" : : "r"(offset) );
 }
-static inline void loadidtr(phys_addr_t base,uint16_t limit)
+static force_inline void loadidtr(phys_addr_t base,uint16_t limit)
 {
 #pragma pack(push,1)
     struct{
@@ -40,17 +49,17 @@ static inline void loadidtr(phys_addr_t base,uint16_t limit)
 #pragma pack(pop)
 }
 //clear interrupt flag.
-static inline void clearinterruptflag(){
+static force_inline void clearinterruptflag(){
     __asm__ __volatile__("cli": : :"memory");
 }
 //set interrupt flag.
-static inline void setinterruptflag(){
+static force_inline void setinterruptflag(){
     __asm__ __volatile__("sti": : :"memory");
 }
-static inline void simplebarrior(){
+static force_inline void nop(){
     __asm__ __volatile__("nop": : :"memory");
 }
-static inline void mfence(){
+static force_inline void mfence(){
     __asm__ __volatile__("mfence": : :"memory");
 }
 #endif
