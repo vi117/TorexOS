b2e189a43f5c252052ba21b5bb8e4beac86ddead
monoid <qaswedfr55@gmail.com>
2019-08-23 11:12:22 +0000

    [ADD] cmos time

---
+++ kernel/include/drivers/timer/cmos.h
@@ -0,0 +1,24 @@
+#include<stdint.h>
+
+namespace CMOS
+{
+inline auto BcdToBinary(uint8_t x) -> uint8_t
+{
+    return ((x >> 4) * 10) + (x & 0x0F);
+}
+
+struct CMOS_RTC
+{
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    uint16_t year;
+    uint8_t month;
+    uint8_t dayOfMonth;
+    uint8_t dayOfWeek;
+    
+    void update();
+    
+    const char *dayOfWeekToCString();
+};
+} // namespace CMOS
---
+++ kernel/src/drivers/timer/cmos.cpp
@@ -0,0 +1,48 @@
+#include<drivers/timer/cmos.h>
+#include<intrins.h>
+#include<util/interrupt_lock.h>
+
+enum RTC
+{
+	CMOSAddress = 0x70,
+	CMOSData = 0x71,
+
+	AddressSecond = 0x00,
+	AddressMinute = 0x02,
+	AddressHour = 0x04,
+	AddressDayOfWeek = 0x06,
+	AddressDayOfMonth = 0x07,
+	AddressMonth= 0x08,
+	AddressYear = 0x09,
+};
+
+void CMOS::CMOS_RTC::update()
+{
+	interrupt_lock t;
+	t.lock();
+	outbyte(CMOSAddress, AddressSecond);
+	second = BcdToBinary(inbyte(CMOSData));
+	outbyte(CMOSAddress, AddressMinute);
+	minute = BcdToBinary(inbyte(CMOSData));
+	outbyte(CMOSAddress, AddressHour);
+	hour = BcdToBinary(inbyte(CMOSData));
+
+	outbyte(CMOSAddress, AddressDayOfWeek);
+	dayOfWeek = BcdToBinary(inbyte(CMOSData));
+	outbyte(CMOSAddress, AddressDayOfMonth);
+	dayOfMonth = BcdToBinary(inbyte(CMOSData));
+	outbyte(CMOSAddress, AddressMonth);
+	month = BcdToBinary(inbyte(CMOSData));
+	outbyte(CMOSAddress, AddressYear);
+	year = BcdToBinary(inbyte(CMOSData)) + 2000;
+	t.unlock();
+}
+
+const char * CMOS::CMOS_RTC::dayOfWeekToCString()
+{
+	static char * dayOfWeekStringTable[8] = { "Error", "Sunday","Monday",
+		"Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+	if (dayOfWeek >= 8)
+		return dayOfWeekStringTable[0];
+	return dayOfWeekStringTable[dayOfWeek];
+}
