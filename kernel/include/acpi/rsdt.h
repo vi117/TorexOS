ca544fdc6abca6492de65f89ce4db96e1230f568
monoid <qaswedfr55@gmail.com>
2019-08-23 10:24:32 +0000

    [ADD] apci base

---
+++ kernel/include/acpi/rsdt.h
@@ -0,0 +1,49 @@
+#pragma once
+#include<stdint.h>
+#include<memory/address.h>
+#include<raw_ostream.h>
+
+namespace acpi
+{
+	struct RSDPDescriptor {
+		char Signature[8];
+		uint8_t Checksum;
+		char OEMID[6];
+		uint8_t Revision;// if it's zero, use rsdt. otherwise, use xsdt.
+		uint32_t RsdtAddress;
+	};
+
+	struct RSDPDescriptor20 : public RSDPDescriptor{
+		uint32_t Length;
+		phys_addr_t XsdtAddress;
+		uint8_t ExtendedChecksum;
+		uint8_t reserved[3];
+	};
+
+	struct SDTHeader {
+		char Signature[4];
+		uint32_t Length;
+		uint8_t Revision;
+		uint8_t Checksum;
+		char OEMID[6];
+		char OEMTableID[8];
+		uint32_t OEMRevision;
+		uint32_t CreatorID;
+		uint32_t CreatorRevision;
+	};
+
+	struct RSDT : public SDTHeader{
+		uint32_t other_sdt_ptr[0];
+		size_t entry_count() const { return (Length - sizeof(SDTHeader)) / 4;}
+	};
+	struct XSDT : public SDTHeader{
+		phys_addr_t other_sdt_ptr[0];
+		size_t entry_count() const { return (Length - sizeof(SDTHeader)) / 8;}
+	};
+
+	const RSDPDescriptor * findRSDP();
+
+	bool doCheckSum(SDTHeader *);
+
+	void printSDTs(text::raw_ostream & os);
+}
---
+++ kernel/src/acpi/rsdt.cpp
@@ -0,0 +1,62 @@
+#include <acpi/rsdt.h>
+#include <stringutil.h>
+
+static constexpr phys_addr_t ebda_location = physical_location(0x40e);
+static constexpr phys_addr_t bios_area = physical_location(0xe0000);
+static constexpr size_t bios_area_length = (0xfffff - 0xe0000);
+const acpi::RSDPDescriptor * acpi::findRSDP(){
+    const uintptr_t ebda_seg = *ebda_location.to_ker().to_ptr_of<uint16_t>();
+    static const char signature[] = "RSD PTR ";
+    const char * ebda = physical_location(ebda_seg << 4).to_ker().to_ptr_of<const char>();
+    for(size_t i = 0; i < 1_KB; i += 16){
+        if(text::memcmp(&ebda[i],signature,8) == 0)
+        {
+            return reinterpret_cast<const RSDPDescriptor *>(&ebda[i]);
+        }
+    }
+    const char * const bios = bios_area.to_ker().to_ptr_of<char>();
+    for(size_t i = 0; i < bios_area_length; i += 16){
+        if(text::memcmp(&bios[i],signature,8) == 0)
+        {
+            return reinterpret_cast<const RSDPDescriptor *>(&bios[i]);
+        }
+    }
+    return nullptr;
+}
+
+bool acpi::doCheckSum(SDTHeader * tableHeader)
+{
+	uint8_t sum = 0;
+
+	for (size_t i = 0; i < tableHeader->Length; i++)
+	{
+		sum += ((uint8_t *)tableHeader)[i];
+	}
+
+	return (sum == 0);
+}
+
+void acpi::printSDTs(text::raw_ostream & os){
+    auto rsd_ptr = findRSDP();
+    if (rsd_ptr->Revision < 2){
+        auto rsdt = physical_location(rsd_ptr->RsdtAddress)
+            .to_ker().to_ptr_of<acpi::RSDT>();
+        for(size_t i = 0; i < rsdt->entry_count(); i++){
+            auto header = physical_location(rsdt->other_sdt_ptr[i])
+                .to_ker().to_ptr_of<acpi::SDTHeader>();
+            os.write(header->Signature,4);
+            os << "\n";
+        }
+    }
+    else
+    {
+        auto xsdt = physical_location(rsd_ptr->RsdtAddress)
+            .to_ker().to_ptr_of<acpi::XSDT>();
+        for(size_t i = 0; i < xsdt->entry_count(); i++){
+            auto header = xsdt->other_sdt_ptr[i]
+                .to_ker().to_ptr_of<acpi::SDTHeader>();
+            os.write(header->Signature,4);
+            os << "\n";
+        }
+    }
+}
