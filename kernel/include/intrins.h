780c345f6e7c42b71982b0b9c51cfa85a33715bc
monoid <qaswedfr55@gmail.com>
2019-09-13 04:48:34 +0000

    [ADD] wmsr rdmsr

--- kernel/include/intrins.h
+++ kernel/include/intrins.h
@@ -89,4 +89,24 @@ static inline void flush_tlb_single(uintptr_t addr)
 {
    asm volatile("invlpg (%0)" ::"r" (addr) : "memory");
 }
+static inline void wrmsr(uint64_t msr, uint64_t value)
+{
+	uint32_t low = value & 0xFFFFFFFF;
+	uint32_t high = value >> 32;
+	asm volatile (
+		"wrmsr"
+		:
+		: "c"(msr), "a"(low), "d"(high)
+	);
+}
+static inline uint64_t rdmsr(uint64_t msr)
+{
+	uint32_t low, high;
+	asm volatile (
+		"rdmsr"
+		: "=a"(low), "=d"(high)
+		: "c"(msr)
+	);
+	return ((uint64_t)high << 32) | low;
+}
 #endif
